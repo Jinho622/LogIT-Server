15a24c6ab0e3a8de711e46d1a38db493d8301cab
Jinho622 <aceho21@gmail.com>
2025-06-12 21:00:15 +0900

    start project

---
+++ src/main/java/com/example/LogIT_Server/LogItServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogIT_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogItServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogItServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LogIT_Server/LogItServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogIT_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogItServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
