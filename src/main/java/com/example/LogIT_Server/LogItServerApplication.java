0f1df8b586ca4722c126611e2f90e7a809089228
Jinho622 <aceho21@gmail.com>
2025-06-13 11:04:32 +0900

    fix!!!

--- src/main/java/com/example/LogIT_Server/LogItServerApplication.java
+++ src/main/java/com/example/LogIT_Server/LogItServerApplication.java
@@ -3,8 +3,6 @@ package com.example.LogIT_Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 삭제할 줄1
-// 삭제할 줄2
 @SpringBootApplication
 public class LogItServerApplication {
     public static void main(String[] args) {
@@ -12,6 +10,8 @@ public class LogItServerApplication {
 
 //        int[] array = new int[2];
 //        System.out.println(array[7]);
+
+        // 추라된줄1
     }
 
 }
